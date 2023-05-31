687bc21c627f8b0e06b72df6240f51ac234781b8
duck <godqhr2256@gmail.com>
2023-05-31 23:48:08 +0900

    chore: 코드 정리

--- client/client.tsx
+++ client/client.tsx
@@ -4,12 +4,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
-
-import App from './layouts/App';
+import App from './src/App';
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL =
-  process.env.NODE_ENV === 'production' ? 'https://sleact.nodebird.com' : 'http://localhost:3090';
+axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? 'http://localhost:3090' : 'http://localhost:3090';
 
 render(
   <BrowserRouter>
