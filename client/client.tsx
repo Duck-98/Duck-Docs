a12eafd46797cecedae0b1a2f4b69fa2a5442d87
duck <godqhr2256@gmail.com>
2023-06-02 00:00:37 +0900

    feat: 전역 스타일 설정

--- client/client.tsx
+++ client/client.tsx
@@ -4,13 +4,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
-import App from './src/App';
+import App from '@/App';
+import GlobalStyle from '@/styles/GlobalStyle';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? 'http://localhost:3090' : 'http://localhost:3090';
 
 render(
   <BrowserRouter>
+    <GlobalStyle />
     <App />
   </BrowserRouter>,
   document.querySelector('#app'),
