71c16cf1a2ce763ac9a0062475e3933646fcd6ae
duck <godqhr2256@gmail.com>
2023-06-02 01:09:40 +0900

    feat: [frontend] socket 연결

--- client/src/components/TextEditor.tsx
+++ client/src/components/TextEditor.tsx
@@ -1,17 +1,53 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { io } from 'socket.io-client';
 import { Quill } from 'react-quill';
 import 'quill/dist/quill.snow.css';
 import * as Styled from '@/styles/TextEditor.styles';
 
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  ['bold', 'italic', 'underline'],
+  [{ color: [] }, { background: [] }],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ align: [] }],
+  ['image', 'blockquote', 'code-block'],
+  ['clean'],
+];
+
 export const TextEditor = () => {
-  const wrapperRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+  const [socket, setSocket] = useState<any>();
+  const [quill, setQuill] = useState<any>();
+  // const wrapperRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    const s = io('http://localhost:3095');
+    setSocket(s);
+    return () => {
+      s.disconnect();
+    };
+  });
+
+  useEffect(() => {
+    quill.on('text-change', (delta: any, oldDelta: any, source: any) => {
+      if (source !== 'user') return;
+    });
+  }, []);
+
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+
+    wrapper.innerHTML = '';
     const editor = document.createElement('div');
-    if (wrapperRef.current) {
-      wrapperRef.current.append(editor);
-      new Quill('.container', { theme: 'snow' });
-    }
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: 'snow',
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    q.disable();
+    q.setText('Loading...');
+    setQuill(q);
   }, []);
 
   return <Styled.Container className="container" ref={wrapperRef}></Styled.Container>;
