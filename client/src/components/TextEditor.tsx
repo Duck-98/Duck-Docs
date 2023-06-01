72ed07ad2f2281eb5cd17b38c64c19861ed4d6be
duck <godqhr2256@gmail.com>
2023-06-02 00:01:18 +0900

    feat: react-quill 적용

--- client/src/components/TextEditor.tsx
+++ client/src/components/TextEditor.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { Quill } from 'react-quill';
+import 'quill/dist/quill.snow.css';
+import * as Styled from '@/styles/TextEditor.styles';
 
 export const TextEditor = () => {
-  return <div>TextEditor</div>;
+  const wrapperRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const editor = document.createElement('div');
+    if (wrapperRef.current) {
+      wrapperRef.current.append(editor);
+      new Quill('.container', { theme: 'snow' });
+    }
+  }, []);
+
+  return <Styled.Container className="container" ref={wrapperRef}></Styled.Container>;
 };
