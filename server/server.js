ec22de3fcf2ada09f00323c3f810b0e8560ecae4
duck <godqhr2256@gmail.com>
2023-06-02 20:34:40 +0900

    feat: [backend] id에 따른 통신 기능

--- server/server.js
+++ server/server.js
@@ -6,8 +6,15 @@ const io = require("socket.io")(3095, {
 });
 
 io.on("connection", (socket) => {
-  socket.on("send-change", (delta) => {
-    socket.broadcast.emit("receive-change", delta);
+  socket.on("get-document", (documentId) => {
+    const data = "";
+    console.log(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", data);
+    socket.on("send-change", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-change", delta);
+    });
   });
+
   console.log("연결되었습니다.");
 });
