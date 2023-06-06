a519be291da00d7d06353e3c59775ecef95d3f75
duck <godqhr2256@gmail.com>
2023-06-07 01:05:18 +0900

    feat: [frontend] 도구 변경 함수 적용 & 크기조절 기능 구현

--- client/src/components/SketchBoard.tsx
+++ client/src/components/SketchBoard.tsx
@@ -1,36 +1,37 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { Stage, Layer, Line } from 'react-konva';
 import { KonvaEventObject } from 'konva/lib/Node';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { changeTool } from '@utils/Tool';
 
 const SketchBoard: React.FC = () => {
   const [lines, setLines] = useState<any[]>([]);
-  const [tool, setTool] = React.useState('pen');
+  const [tool, setTool] = useState<string>('pencil');
   const [currentColor, setCurrentColor] = useState<string>('black');
+  const [size, setSize] = useState<number>(3);
+
   const isDrawing = useRef<boolean>(false);
-  const linesRef = useRef<any[]>([]);
 
   const handleMouseDown = (event: any) => {
     isDrawing.current = true;
     const { x, y } = event.target.getStage().getPointerPosition();
-    linesRef.current.push({
-      points: [x, y],
-      color: currentColor,
-      tool,
-    });
+    setLines([...lines, { tool, color: currentColor, points: [x, y], size }]);
   };
 
   const handleMouseMove = (event: any) => {
-    if (!isDrawing.current) return;
-    const { x, y } = event.target.getStage().getPointerPosition();
-    console.log(x, y);
-    const newLines = [...linesRef.current];
-    const lastLine = newLines[newLines.length - 1];
+    if (!isDrawing.current) {
+      return;
+    }
+    const stage = event.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = lines[lines.length - 1];
+    // add point
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
 
-    lastLine.points = lastLine.points.concat([x, y]);
-    newLines.splice(newLines.length - 1, 1, lastLine);
-    setLines(newLines);
-    linesRef.current = newLines;
-    console.log(lines);
+    // replace last
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.concat());
   };
 
   const handleMouseUp = () => {
@@ -43,32 +44,43 @@ const SketchBoard: React.FC = () => {
   };
 
   const handleCleanAll = () => {
-    linesRef.current = [];
     setLines([]);
   };
 
-  const handleCleanSelect = () => {
-    // setIsErasing(true);
-    // lines[0].mode = 'erase';
-    // console.log(lines);
-    setTool('eraser');
+  const handleSizeUp = () => {
+    setSize((prevSize) => prevSize + 1);
+  };
+  const handleSizeDown = () => {
+    if (size === 1) {
+      toast('더 이상 작게 할 수 없습니다.');
+    } else {
+      setSize((prevSize) => prevSize - 1);
+    }
   };
 
+  const handleChangeTool = (e: any) => {
+    setTool(e.target.value);
+  };
   useEffect(() => {
-    return () => {
-      isDrawing.current = false;
-    };
-  }, []);
-
+    changeTool(tool);
+  }, [tool]);
   return (
     <div>
+      <ToastContainer />
       <div>
-        <button onClick={handleCleanSelect}>지우개</button>
         <button onClick={handleCleanAll}>초기화</button>
         <button onClick={() => handleColorChange('black')}>Black</button>
         <button onClick={() => handleColorChange('red')}>Red</button>
         <button onClick={() => handleColorChange('blue')}>Blue</button>
         <button onClick={() => handleColorChange('green')}>Green</button>
+        <button onClick={handleSizeUp}>+</button>
+        <button onClick={handleSizeDown}>-</button>
+        <select value={tool} onChange={(e) => handleChangeTool(e)}>
+          <option value="felt-tip">Felt-Tip</option>
+          <option value="brush">Brush</option>
+          <option value="pencil">Pencil</option>
+          <option value="eraser">Eraser</option>
+        </select>
       </div>
       <Stage
         width={window.innerWidth}
@@ -78,18 +90,22 @@ const SketchBoard: React.FC = () => {
         onMouseUp={handleMouseUp}
       >
         <Layer>
-          {lines.map((line: any, i: number) => (
-            <Line
-              key={i}
-              points={line.points}
-              stroke={line.color}
-              strokeWidth={5}
-              tension={0.5}
-              lineCap="round"
-              lineJoin="round"
-              globalCompositeOperation={line.tool === 'eraser' ? 'destination-out' : 'source-over'}
-            />
-          ))}
+          {lines.map((line: any, i: number) => {
+            const toolSettings = changeTool(line?.tool);
+            return (
+              <Line
+                key={i}
+                points={line.points}
+                stroke={line.color}
+                strokeWidth={line.size}
+                tension={0.5}
+                shadowBlur={toolSettings.shadowBlur}
+                lineCap={toolSettings.lineCap}
+                lineJoin={toolSettings.lineJoin}
+                globalCompositeOperation={line.tool === 'eraser' ? 'destination-out' : 'source-over'}
+              />
+            );
+          })}
         </Layer>
       </Stage>
     </div>
