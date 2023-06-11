0ba2fcfaff0aad7dad05aedfb9e01a9078a73330
duck <godqhr2256@gmail.com>
2023-06-11 23:45:09 +0900

    feat: [frontend] Color-picker 개발

--- client/src/components/ColorPicker.tsx
+++ client/src/components/ColorPicker.tsx
@@ -1,69 +1,95 @@
-import React, { useEffect, useRef } from 'react';
-import { Group, Rect, Stage } from 'react-konva';
+import React, { useState, useEffect } from 'react';
+import styled, { css } from 'styled-components';
+import { ChromePicker } from 'react-color';
 
-interface ColorSquareType {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-  color: string;
-  onClick: (color: string) => void;
+interface ColorPickerProps {
+  open: boolean;
+  setCurrentColor: React.Dispatch<React.SetStateAction<string>>;
+  currentColor: string;
+  handleColorChange: (color: string) => void;
+  handleOpenPicker: () => void;
 }
 
-interface ColorPickerType {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-  colors: string[];
-  onChange: any;
-}
+const ColorPicker = ({
+  setCurrentColor,
+  currentColor,
+  handleColorChange,
+  open,
+  handleOpenPicker,
+}: ColorPickerProps) => {
+  //   const [open, setOpen] = useState<boolean>(false);
 
-function ColorSquare({ x, y, width, height, color, onClick }: ColorSquareType) {
-  const shapeRef = useRef<any>(null);
-  const shape = shapeRef.current;
+  //   const handleOpenPicker = () => {
+  //     setOpen((prev) => !prev);
+  //     console.log(open);
+  //   };
 
-  const handleMouseEnter = () => {
-    const stage = shape.getStage();
-    stage.container().style.cursor = 'pointer';
-  };
-
-  const handleMouseLeave = () => {
-    const stage = shape.getStage();
-    stage.container().style.cursor = 'default';
-  };
-
-  const handleClick = () => {
-    onClick(color);
-  };
+  useEffect(() => {
+    if (!currentColor) {
+      setCurrentColor('');
+    }
+    setCurrentColor(currentColor);
+  }, [currentColor]);
 
   return (
-    <Stage
-      width={window.innerWidth}
-      height={window.innerHeight}
-      onMouseDown={handleClick}
-      onMouseMove={handleMouseEnter}
-      onMouseUp={handleMouseLeave}
-    >
-      <Rect x={x} y={y} width={width} height={height} fill={color} ref={shapeRef} />
-    </Stage>
+    <>
+      <Box>
+        {/* <Input value={currentColor} onChange={(e) => handleColorChange(e.target.value)} /> */}
+        <Color color={currentColor} onClick={handleOpenPicker}></Color>
+        <PickerContainer open={open}>
+          <ChromePicker
+            className="picker"
+            color={currentColor}
+            onChange={(currentColor) => handleColorChange(currentColor.hex)}
+          />
+        </PickerContainer>
+      </Box>
+    </>
   );
-}
+};
 
-export default function ColorsPicker({ x, y, width, height, colors, onChange }: ColorPickerType) {
-  return (
-    <Group x={x} y={y}>
-      {colors.map((color, idx) => (
-        <ColorSquare
-          key={colors[idx]}
-          x={0}
-          y={idx * height}
-          width={width}
-          height={height}
-          color={colors[idx]}
-          onClick={onChange}
-        />
-      ))}
-    </Group>
-  );
-}
+export default ColorPicker;
+
+const Color = styled.div`
+  background: ${(props) => props.color || 'black'};
+  height: 40px;
+  width: 40px;
+  border-radius: 6px;
+`;
+
+const PickerContainer = styled.div<{ open: boolean }>`
+  position: absolute;
+  top: 60%;
+  left: 80px;
+  z-index: 100;
+
+  ${({ open }) =>
+    open
+      ? css`
+          display: block;
+          opacity: 1;
+          transition: all 0.1s ease-in-out;
+        `
+      : css`
+          display: none;
+          opacity: 0;
+          transition: all 0.3s ease-in-out;
+        `};
+`;
+
+const Box = styled.div`
+  padding: 8px;
+`;
+
+const Input = styled.input`
+  border-radius: 14px;
+  height: 58px;
+  padding: 0 21px;
+  font-size: 16px;
+  transition: box-shadow 0.1s;
+  box-shadow: inset #d8d8da 0 0 0 1px, inset white 0 0 0 100px !important;
+  border: none;
+  width: 100%;
+  color: black !important;
+  font-family: 'Inter', sans-serif;
+`;
